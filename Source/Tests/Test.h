8cfa60a71fdab19a57508e0dc911bf0690b7253f
Sakthivel Ganesan <56190146+sms2sakthivel@users.noreply.github.com>
2019-10-15 00:54:10 +0530

    CTL-8 Simple Test API has added

---
+++ Source/Tests/Test.h
@@ -0,0 +1,21 @@
+/*
+ * Test.h
+ *
+ *  Created on: Oct 14, 2019
+ *      Author: Sakthivel Ganesan
+ */
+
+#ifndef _TEST_
+#define _TEST_
+#include <iostream>
+namespace Test
+{
+	class Test {
+	public:
+		void TestList();
+		void TestLinkedList();
+	};
+}
+
+
+#endif /* _TEST_ */
