862a537735a55b3903f058987f4086dc3378eb7a
Sakthivel Ganesan <56190146+sms2sakthivel@users.noreply.github.com>
2019-10-15 01:00:42 +0530

    CTL-9 Simple Test API has added for Linkedlist

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include "Tests/Test.h"
+#include <ctime>
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello World!!!" << std::endl;
+	std::clock_t time = std::clock();
+	std::cout << "Time = " << time << std::endl;
+	std::cout << "Welcome Everyone!" << std::endl;
+	Test::Test test;
+	test.TestList();
+	test.TestLinkedList();
+	std::cout << "Time = " << std::clock() << std::endl;
+	std::cout << std::clock() - time << std::endl;
 	return 0;
 }
